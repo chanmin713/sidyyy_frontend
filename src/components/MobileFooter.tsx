9bd286b0b6cbcf0b0c896db50c33e3885dcb7c16
chanmin713 <chanmin713@snu.ac.kr>
2025-10-24 22:57:41 +0900

    Revert padding and increase gap between footer links

--- src/components/MobileFooter.tsx
+++ src/components/MobileFooter.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export function MobileFooter() {
   return (
     <footer className="border-t mt-8">
-      <div className="max-w-6xl mx-auto px-6 py-4">
+      <div className="max-w-6xl mx-auto px-4 py-4">
         <div className="flex flex-col gap-6">
           {/* 브랜드 정보 */}
           <div className="text-left px-2">
@@ -17,7 +17,7 @@ export function MobileFooter() {
           </div>
 
           {/* 링크들 */}
-          <div className="flex flex-wrap justify-center items-center gap-2 text-xs text-gray-600 px-2">
+          <div className="flex flex-wrap justify-center items-center gap-4 text-xs text-gray-600 px-2">
             <a href="#" className="hover:text-gray-900 px-2 py-0.5">문의하기</a>
             <span className="text-gray-400">|</span>
             <a href="#" className="hover:text-gray-900 px-2 py-0.5">이용약관</a>
