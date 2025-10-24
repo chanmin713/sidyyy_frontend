4636335befe074679b113f625274d5adf1c95ee8
chanmin713 <chanmin713@snu.ac.kr>
2025-10-24 23:23:50 +0900

    style: increase horizontal padding for content area

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -15,7 +15,7 @@ export function Layout({ children }: LayoutProps) {
       <div className="max-w-6xl mx-auto h-full">
         <main className="flex justify-center gap-x-8 pb-3 md:px-5 md:pb-5">
           {/* 중앙 콘텐츠 */}
-          <div className="w-full lg:w-3/5 pb-0 md:pb-0 bg-white px-4 md:px-6">
+          <div className="w-full lg:w-3/5 pb-0 md:pb-0 bg-white px-6 md:px-8">
             {children}
           </div>
           
