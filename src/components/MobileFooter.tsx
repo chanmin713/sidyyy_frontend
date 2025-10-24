fe4149f61a79cb565f83c1e7e42a49dea6f69f52
chanmin713 <chanmin713@snu.ac.kr>
2025-10-24 23:02:48 +0900

    style: make footer links equal width with flex-1 and center alignment

--- src/components/MobileFooter.tsx
+++ src/components/MobileFooter.tsx
@@ -18,13 +18,13 @@ export function MobileFooter() {
 
           {/* 링크들 */}
           <div className="flex justify-center items-center gap-3 text-xs text-gray-600 px-2 whitespace-nowrap">
-            <a href="#" className="hover:text-gray-900 px-2 py-0.5">문의하기</a>
+            <a href="#" className="hover:text-gray-900 px-2 py-0.5 flex-1 text-center">문의하기</a>
             <span className="text-gray-400">|</span>
-            <a href="#" className="hover:text-gray-900 px-2 py-0.5">이용약관</a>
+            <a href="#" className="hover:text-gray-900 px-2 py-0.5 flex-1 text-center">이용약관</a>
             <span className="text-gray-400">|</span>
-            <a href="#" className="hover:text-gray-900 px-2 py-0.5">개인정보처리방침</a>
+            <a href="#" className="hover:text-gray-900 px-2 py-0.5 flex-1 text-center">개인정보처리방침</a>
             <span className="text-gray-400">|</span>
-            <a href="#" className="hover:text-gray-900 px-2 py-0.5">서비스 소개</a>
+            <a href="#" className="hover:text-gray-900 px-2 py-0.5 flex-1 text-center">서비스 소개</a>
           </div>
         </div>
 
